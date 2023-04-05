cffcb4d9ddbf4f6b2b76c5678bd3cd710dc04319
Howard Huang <hao@hwrd.me>
2023-04-04 21:19:48 -0700

    Fix Firefox with Wayland config

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-if [ $(uname) = 'Linux' ]; then
-  ln -sf "${PWD}/config" "${HOME}/.pam_environment"
+if [ "$(uname)" = 'Linux' ]; then
+	CONFIG="${HOME}/.config/environment.d/envvars.conf"
+	CONTENTS="MOZ_ENABLE_WAYLAND=1"
+	if grep "$CONTENTS" "$CONFIG" >/dev/null 2>&1; then
+		echo "Firefox config is already installed."
+	else
+		echo "$CONTENTS" >>"$CONFIG"
+	fi
 fi
