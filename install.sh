6892314a2912e547c06c34e4f906e81f61beda73
Howard Huang <hao@hwrd.me>
2023-04-04 22:18:16 -0700

    Add Tridactyl configuration

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 if [ "$(uname)" = 'Linux' ]; then
-	CONFIG="${HOME}/.config/environment.d/envvars.conf"
+	ENVVAR_FILE="${HOME}/.config/environment.d/envvars.conf"
 	CONTENTS="MOZ_ENABLE_WAYLAND=1"
-	if grep "$CONTENTS" "$CONFIG" >/dev/null 2>&1; then
-		echo "Firefox config is already installed."
-	else
-		echo "$CONTENTS" >>"$CONFIG"
+	if ! grep "$CONTENTS" "$ENVVAR_FILE" >/dev/null 2>&1; then
+		echo "$CONTENTS" >>"$ENVVAR_FILE"
 	fi
 fi
+
+CONFIG_DIR="${HOME}/.config/tridactyl"
+THEME_DIR="${CONFIG_DIR}/themes"
+mkdir -p "${THEME_DIR}"
+ln -sf "${PWD}/tridactylrc" "${CONFIG_DIR}"
+ln -sf "${PWD}/catppuccin.css" "${THEME_DIR}"
+echo "Don't forget to run :nativeinstall in Tridactyl to be able to source tridactylrc"
