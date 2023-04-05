bd67e711af4b7ddac9178ae792ebc57e88d3aaa6
Howard Huang <hao@hwrd.me>
2023-04-04 22:32:38 -0700

    Add instructions for macOS Tridactyl setup

--- install.sh
+++ install.sh
@@ -14,3 +14,7 @@ mkdir -p "${THEME_DIR}"
 ln -sf "${PWD}/tridactylrc" "${CONFIG_DIR}"
 ln -sf "${PWD}/catppuccin.css" "${THEME_DIR}"
 echo "Don't forget to run :nativeinstall in Tridactyl to be able to source tridactylrc"
+
+if [ "$(uname)" = 'Darwin' ]; then
+	echo ":nativeinstall may not work on macOS. Check out https://github.com/tridactyl/tridactyl/issues/4298 for a workaround"
+fi
